244026cd13ab55d4287c4b64486e0c3fe20dda1d
dreax99 <dreax99@gmail.com>
2024-12-11 10:32:36 +0100

    Views, urls configurations

--- backendDj/backendDj/urls.py
+++ backendDj/backendDj/urls.py
@@ -1,22 +1,21 @@
-"""
-URL configuration for backendDj project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path 
+from todo.views import TodoViewSet, CreateUserView 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Token
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("api-auth/", include("rest_framework.urls")),  # Login e logout per l'interfaccia amministrativa di Django.
+    
+    #User 
+    path('api/user/register/', CreateUserView.as_view(), name='create_user'),
+    
+    #TODOs
+    path('api/todo/', TodoViewSet.as_view({'get': 'list', 'post': 'create'}), name='todo'),
+    
 ]
--- backendDj/todo/views.py
+++ backendDj/todo/views.py
@@ -4,26 +4,31 @@ from .serializers import TodoSerializer, UserSerializer
 from rest_framework import viewsets, permissions, generics
 from django.contrib.auth.models import User
 
+"""
+ Create your views here.
+ Le viste gestiscono le richieste API e utilizzano il serializer per restituire i dati.
 
-# Create your views here.
-# Le viste gestiscono le richieste API e utilizzano il serializer per restituire i dati.
+ In Django REST Framework (DRF), una "view" è il livello che collega i dati del backend (modelli e serializer) 
+ con i metodi HTTP (GET, POST, PUT, DELETE, ecc.). 
+ La vista elabora le richieste API, utilizza il serializer per serializzare o deserializzare i dati e restituisce una risposta
 
-# In Django REST Framework (DRF), una "view" è il livello che collega i dati del backend (modelli e serializer) 
-# con i metodi HTTP (GET, POST, PUT, DELETE, ecc.). 
-# La vista elabora le richieste API, utilizza il serializer per serializzare o deserializzare i dati e restituisce una risposta
+ModelViewSet è una classe predefinita di DRF 
+ che combina tutte le operazioni CRUD in una singola classe di vista. Ti consente di eseguire le seguenti operazioni: 
+ Get, Post, Put, Delete.
 
-
-#ModelViewSet è una classe predefinita di DRF 
-# che combina tutte le operazioni CRUD in una singola classe di vista. Ti consente di eseguire le seguenti operazioni: 
-# Get, Post, Put, Delete.
-
+queryset:
+Contiene gli oggetti che verranno gestiti dalla vista.
+Può essere personalizzato per filtrare o ordinare i dati.
+Esempio: queryset = Todo.objects.filter(completed=False) (solo attività incomplete). 
+"""
 
 class TodoViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     
+    
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.AllowAny,) # Permette a chiunque di creare un nuovo utente.
+    permission_classes = (permissions.AllowAny,)  #Permette a chiunque di creare un nuovo utente.
       
